629c27dc840812af95d9bd47e526e6538d2ab7c1
John Sanders <jbsaners92@gmail.com>
2024-09-16 05:22:20 -0600

    first commit! changed picture and text to a base react typescript project

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import squirtles from './squirtles.webp'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={squirtles} className="selfie" alt="selfie" />
+        <p>
+          Sup bruh I'm John
+          <br></br>
+          (A work in progress)
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
